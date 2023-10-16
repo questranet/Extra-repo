3b0a965a94aefb4ea3610019fb0917cebfaa0b49
Adewale Megbabi <137506455+questranet@users.noreply.github.com>
2023-10-16 20:54:55 +0100

    Initial commit

---
+++ Jenkins-ip/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket = "waleapagun-bucket"
+    key    = "repo/Jenkins-ip/terraform.tfstate"
+    region = "us-east-1"
+
+  }
+}
+
+data "aws_instance" "instance" {
+  instance_id = "i-07431c766b24cd39b"
+}
+
+resource "aws_route53_record" "jenkins" {
+  name    = "jenkins.waleapagun.online"
+  type    = "A"
+  zone_id = "Z08999912AI7EUJ47AGDO"
+  records = [data.aws_instance.instance.public_ip]
+  ttl = 10
+}
+
+
+
+
+
+
