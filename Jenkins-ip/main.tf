3e425f73b61517f321ecb048073eda080c2c5a31
Wale <Adewale.megbabi@gmail.com>
2023-10-18 10:08:40 +0100

    Initial commit

--- Jenkins-ip/main.tf
+++ Jenkins-ip/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "waleapagun-bucket"
-    key    = "Extra-repo/Jenkins-ip/terraform.tfstate"
+    key    = "repo/Jenkins-ip/terraform.tfstate"
     region = "us-east-1"
 
   }
