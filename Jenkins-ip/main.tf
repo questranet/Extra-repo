e5b1950373895c4a6d91cdec2e4f1ed9016c2130
Wale <Adewale.megbabi@gmail.com>
2023-10-19 20:25:10 +0100

    Initial commit

--- Jenkins-ip/main.tf
+++ Jenkins-ip/main.tf
@@ -19,6 +19,20 @@ resource "aws_route53_record" "jenkins" {
   ttl = 10
 }
 
+data "aws_instance" "artifactory" {
+  instance_id = " i-03ad349494d920641"
+}
+
+resource "aws_route53_record" "artifactory" {
+  name    = "artifactory.waleapagun.online"
+  type    = "A"
+  zone_id = "Z08999912AI7EUJ47AGDO"
+  records = [data.aws_instance.artifactory.public_ip]
+  ttl = 10
+}
+
+
+
 
 
 
